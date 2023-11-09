813b0ec6b036838bda10febea654c9c503d72bb2
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-09 16:26:02 +0100

    ADD error handling if entered date is anterior to today's

--- src/routes/CreateSecretSanta/CreateSecretSanta.jsx
+++ src/routes/CreateSecretSanta/CreateSecretSanta.jsx
@@ -5,10 +5,14 @@ export default function CreateSecretSanta() {
   const [participants, setParticipants] = useState(
     Array.from({ length: 2 }, () => ({ email: '', firstName: '' }))
   )
+  const [eventName, setEventName] = useState('')
+  const [eventDesc, setEventDesc] = useState('')
+  const [eventDate, setEventDate] = useState('')
+  const today = new Date().toISOString().split('T')[0]
 
   const incrementTotal = () => {
     setTotal(total + 1)
-    setParticipants([...participants, { email: '', firstName: '' }])
+    setParticipants([...participants], { email: '', firstName: '' })
   }
 
   const decrementTotal = () => {
@@ -30,13 +34,45 @@ export default function CreateSecretSanta() {
     setParticipants(updatedParticipants)
   }
 
-  console.log(participants)
+  const handleEventDateChange = (e) => {
+    const enteredDate = e.target.value
+
+    if (enteredDate < today) {
+      alert("La date de l'événement ne peut pas être antérieure à aujourd'hui.")
+    } else {
+      setEventDate(enteredDate)
+    }
+  }
 
   return (
     <>
       <h1>Créer un secret santa</h1>
       <div>
         <h2>Description de l'évènement</h2>
+        <input
+          type="text"
+          name="event-name"
+          id="event-name"
+          placeholder="Nom de l'évènement"
+          value={eventName}
+          onChange={(e) => setEventName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="event-desc"
+          id="event-desc"
+          placeholder="Description"
+          value={eventDesc}
+          onChange={(e) => setEventDesc(e.target.value)}
+        />
+        <input
+          type="date"
+          name="event-date"
+          id="event-date"
+          placeholder="Date de l'événement"
+          value={eventDate}
+          onChange={handleEventDateChange}
+        />
       </div>
       <div>
         <h2>Nombre de participants</h2>
