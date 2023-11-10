371bb25dd92a50ed91017c025c4dff4082b90c2b
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-10 22:42:55 +0100

    ADD redirection to login

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from '../../components/Button/Button'
 
 import './Header.scss'
+
 export default function Header() {
+  const navigate = useNavigate()
+  const handleLogin = () => {
+    console.log('coucou')
+    navigate('/SignIn')
+  }
   return (
     <header>
       <nav>
-        <Button className="button__color--primary">Login</Button>
+        <Button className="button__color--primary" onClick={handleLogin}>
+          Login
+        </Button>
       </nav>
     </header>
   )
