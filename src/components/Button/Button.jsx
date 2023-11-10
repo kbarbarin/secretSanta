1651a6e44c91b97febe9fde768955b4654088706
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-10 19:50:47 +0100

    CHANGE button appearance

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import './Button.scss';
+import React from 'react'
+import './Button.scss'
 
 export default function Button(props) {
-
-    return (
-        <div className={`button ${props?.className || ""}`} style={props?.style} onClick={props?.onClick}>
-            <h1 className={`button-title ${props?.styleTitle || ""}`}>
-                {props?.children}
-            </h1>
-        </div>
-    );
+  return (
+    <div
+      className={`button ${props?.className || ''}`}
+      style={props?.style}
+      onClick={props?.onClick}
+    >
+      <p className={`button-title ${props?.styleTitle || ''}`}>
+        {props?.children}
+      </p>
+    </div>
+  )
 }
