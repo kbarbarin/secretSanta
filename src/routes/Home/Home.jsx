609e2f8003fdc65062f7dd1f4431ea840a8e9966
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-10 22:15:23 +0100

    CREATE 1rst section of the homepage

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 
-export default function Home ()  {
-    return (
-        <div>
-            <Outlet />
+import './Home.scss'
+
+import Button from '../../components/Button/Button'
+
+export default function Home() {
+  return (
+    <div>
+      <Outlet />
+      <div className="home">
+        <img src="assets/logo.png" alt="secret santa's logo" />
+        <h1 className="home__title">Ho-Ho! </h1>
+        <h1 className="home__title--yellow">It's Secret Santa Time!</h1>
+        <div className="home__buttons">
+          <Button className="button__color--primary">
+            Create my Secret Santa
+          </Button>
+          <Button className="button__color--secondary">
+            Join a Secret Santa
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
