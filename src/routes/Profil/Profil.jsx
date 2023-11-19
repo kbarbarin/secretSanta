4c27e7f82e4b6a9ad1cf195b4aca17df9f7040b7
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-19 16:19:41 +0100

    CREATE new page : profile

--- src/routes/Profil/Profil.jsx
+++ src/routes/Profil/Profil.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react'
 
-export default function Profil () {
-    return (
-        <p>
-            Profil
-        </p>
-    )
+import Button from '../../components/Button/Button'
+import LogoutButton from '../LogOut/Logout'
+
+import './Profile.scss'
+
+export default function Profil() {
+  return (
+    <div className="profile">
+      <h1>My profile</h1>
+      <div className="profile__container">
+        <div className="profile__infos">
+          <img src="/assets/elf.png" alt="elf" />
+          <div className="profile__name">
+            <p className="profile__fname">Lucas</p>
+            <Button className={'button__profile'}>Edit profile</Button>
+          </div>
+        </div>
+        <p className="profile__email">monadressemail@mail.fr</p>
+        {LogoutButton()}
+      </div>
+    </div>
+  )
 }
