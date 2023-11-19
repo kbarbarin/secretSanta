e62460fede50a35dac78fff2295c3fbc087d3798
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-19 17:07:41 +0100

    Redirect to create

--- src/routes/Profil/Profil.jsx
+++ src/routes/Profil/Profil.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
+import { useNavigate } from 'react-router-dom'
+
 import Button from '../../components/Button/Button'
 import LogoutButton from '../LogOut/Logout'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 import './Profile.scss'
 
 export default function Profil() {
+  const sessions = ['mon ss 1', 'mon ss 2', 'mon ss 3']
+  const navigate = useNavigate()
+
+  const redirectToCreate = () => {
+    navigate('/create')
+  }
   return (
     <div className="profile">
       <h1>My profile</h1>
@@ -19,6 +30,21 @@ export default function Profil() {
         </div>
         <p className="profile__email">monadressemail@mail.fr</p>
         {LogoutButton()}
+      </div>
+      <div className="sessions">
+        <h2>My Secret Santa</h2>
+        {sessions.map((session, index) => (
+          <div className="sessions__recap" key={index}>
+            <Button className="button__tertiary">{session}</Button>
+            <FontAwesomeIcon icon={faTrash} style={{ color: '#f5f5f5' }} />
+          </div>
+        ))}
+        <Button
+          className="button__color--primary bottom"
+          onClick={redirectToCreate}
+        >
+          Create a Secret Santa
+        </Button>
       </div>
     </div>
   )
