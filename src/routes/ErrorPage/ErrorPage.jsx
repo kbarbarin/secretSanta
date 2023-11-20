3eede33be85da427d9640d0b16ca9e2057232c1c
Lucas Miranda <lmiranda@corp.free.fr>
2023-11-20 08:40:57 +0100

    UPDATE error-page into errorPage

--- src/routes/ErrorPage/ErrorPage.jsx
+++ src/routes/ErrorPage/ErrorPage.jsx
@@ -14,7 +14,7 @@ export default function ErrorPage() {
   console.error(error)
 
   return (
-    <div id="error-page">
+    <div id="errorPage">
       <img src="/assets/404Logo.png" alt="Error 404" />
       <img
         className="santaError"
