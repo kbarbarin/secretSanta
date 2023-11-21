4b918f724727995c1922d6623d20ddfdc24db4ba
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-21 22:22:31 +0100

    Retrieve today's date

--- src/components/HeaderSummary/HeaderSummary.jsx
+++ src/components/HeaderSummary/HeaderSummary.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 import './HeaderSummary.scss'
 
 export default function HeaderSummary({ name }) {
+  const date = '2023-12-26'
+  const today = new Date()
+  const day = today.getDate()
+  const month = today.getMonth() + 1
+  const year = today.getFullYear()
+  console.log(day, month, year)
+  console.log(today)
+
   return (
     <div className="headerSum">
-      <h3 className="headerSum__title">Secret Santa's name</h3>
+      <h3 className="headerSum__title">{name}</h3>
       <div className="headerSum__container">
         <p className="headerSum__desc">The event will begin in</p>
         <div className="headerSum__date">
