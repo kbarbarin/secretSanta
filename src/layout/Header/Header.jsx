18a63c71ffe7a3b7137d5fba0bcc23daf6e5fb52
Killian Barbarin <killianb31@gmail.com>
2023-11-22 18:36:25 +0100

    FIX conflic

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -43,7 +43,7 @@ export default function Header() {
       setButton('');
       setNavigation("");
   }
-  }, [location])
+  }, [location, isCreateOrSummaryPage])
 
   const handleNavigation = () => {
     navigate(navigation);
