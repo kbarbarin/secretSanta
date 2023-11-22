f913867d00fe1eeccc41e28d1d3190f253cdbc10
Killian Barbarin <killianb31@gmail.com>
2023-11-22 16:02:00 +0100

    ADD dynamic display for the header button

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,22 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { auth } from '../../firebase/Firebase'
 import Button from '../../components/Button/Button'
 
 import './Header.scss'
 import Back from '../../components/Back/Back'
 
 export default function Header() {
-  const [button, setButton] = useState("Login");
+  const [button, setButton] = useState("SIGN IN");
+  const [navigation, setNavigation] = useState("/signin");
   const location = useLocation()
   const navigate = useNavigate()
-  const handleLogin = () => {
-    navigate('/signin')
+
+  const isCreateOrSummaryPage = /^\/(create\/?)?$|^\/create\/\w+$|^\/summary$/i.test(location.pathname);
+
+  useEffect(() => {
+    console.log(location.pathname);
+    if (location.pathname === '/') {
+      setButton('SIGN IN');
+      setNavigation("/signin");
+    }
+    else if (isCreateOrSummaryPage) {
+      setButton('MY PROFILE');
+      setNavigation("/profile");
+    }
+    if (
+      location.pathname === '/quizz' ||
+      location.pathname === '/giftideas' ||
+      location.pathname === '/secretsanta'
+    ) {
+      if (auth.currentUser) {
+        setButton('MY PROFILE');
+        setNavigation("/profile");
+      } else {
+        setButton('SIGN IN');
+        setNavigation("/signin");
+      }
+    }
+  }, [location])
+
+  const handleNavigation = () => {
+    navigate(navigation);
   }
   return (
     <header>
       <nav>
         <Back />
-        <Button className="button__color--primary" onClick={handleLogin}>
+        <Button className="button__color--primary" onClick={handleNavigation}>
           {button}
         </Button>
       </nav>
