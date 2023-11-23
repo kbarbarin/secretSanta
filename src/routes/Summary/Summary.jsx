7f870217d6192f911ef5ba7ebf7830546f84c470
unknown <hassan.iqbal1@outlook.fr>
2023-11-23 19:44:06 +0100

    Add anonymous login

--- src/routes/Summary/Summary.jsx
+++ src/routes/Summary/Summary.jsx
@@ -1,4 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+const SummaryPage = () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        signInAnonymously(auth)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Utilisateur connecté anonymement :", user);
+          })
+          .catch((error) => {
+            console.error("Erreur de connexion anonyme :", error);
+          });
+      }
+    });
+    return () => unsubscribe();
+  }, [auth, navigate]);
+}
 
 export default function Summary () {
     return (
