fce41f32a8e28bf6155d62c5b1af504162f17b05
Killian Barbarin <killianb31@gmail.com>
2023-11-24 14:17:28 +0100

    ADD dynamic version for summary with the good name, description, and the inveted

--- src/routes/Summary/Summary.jsx
+++ src/routes/Summary/Summary.jsx
@@ -11,40 +11,43 @@ import './Summary.scss'
 export default function Summary() {
   const { id, userid } = useParams()
   const [secretSanta, setSecretSanta] = useState({})
+  const [ready, setReady] = useState(0);
 
-  // useEffect(() => {
-  //   const getSecretSanta = async () => {
-  //     const usersCollectionRef = collection(db, 'secretSanta')
-  //     const q = query(usersCollectionRef, where('id', '==', id))
-  //     const querySnapshot = await getDocs(q)
-  //     setSecretSanta(querySnapshot.docs[0].data())
-  //   }
-  //   getSecretSanta()
-  // })
+  useEffect(() => {
+    const getSecretSanta = async () => {
+      const usersCollectionRef = collection(db, 'secretSanta')
+      const q = query(usersCollectionRef, where('id', '==', id))
+      const querySnapshot = await getDocs(q)
+      setSecretSanta(querySnapshot.docs[0].data())
+      console.log(querySnapshot.docs[0].data());
+      console.log(querySnapshot.docs[0].data()?.participants?.filter(obj => obj.isProfilCompleted === true));
+      setReady(querySnapshot.docs[0].data()?.participants?.filter(obj => obj.isProfilCompleted === true));
+    }
+    getSecretSanta()
+  }, [])
 
   return (
     <div className="summary">
       <HeaderCard
-        mainTitle={'Secret Santa’s Name'}
+        mainTitle={secretSanta?.eventName}
         titleDesc={'by Lucas'}
         className="summary__headerCard"
       />
       <HeaderSummary />
       <div className="summary__participants">
         <div className="summary__pill">
-          <p>6</p>
+          <p>{secretSanta?.participants?.length}</p>
           <p>Invited</p>
         </div>
         <div className="summary__pill">
-          <p>3</p>
+          <p>{ready.length}</p>
           <p>Ready</p>
         </div>
       </div>
       <div className="summary_desc">
         <p className="summary__descTitle">Description:</p>
         <p className="summary__descContent">
-          Blablabla and blablabla blablabla. There is blablabla.Thank you for
-          blablabla!
+          {secretSanta?.eventDesc}
         </p>
       </div>
     </div>
