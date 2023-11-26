aa664a4e6219ad66ae9c0e5096966695c383bb5c
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-26 23:10:19 +0100

    ADD firebase call

--- src/routes/Join/Join.jsx
+++ src/routes/Join/Join.jsx
@@ -1,5 +1,9 @@
 import React, { useRef } from 'react'
 
+import { collection, getDocs } from 'firebase/firestore'
+
+import { db } from '../../firebase/Firebase'
+
 import HeaderCard from '../../layout/HeaderCard/HeaderCard'
 import Input from '../../components/Input/Input'
 import Button from '../../components/Button/Button'
@@ -9,8 +13,38 @@ import './Join.scss'
 export default function Join() {
   const sessionIdRef = useRef(null)
 
-  const handleJoinSession = () => {
-    console.log(sessionIdRef.current.value)
+  const handleJoinSession = async () => {
+    const enteredCode = sessionIdRef.current.value
+
+    try {
+      const participantsCollection = await getDocs(
+        collection(db, 'secretSanta')
+      )
+
+      participantsCollection.forEach((participantDoc) => {
+        const participantData = participantDoc.data()
+
+        console.log(participantData.participants)
+
+        if (participantData) {
+          const giftedIdeas = participantData.giftedIdeas
+
+          console.log(giftedIdeas)
+
+          const isCodeValid =
+            giftedIdeas &&
+            giftedIdeas.some((giftedIdea) => giftedIdea.id === enteredCode)
+
+          if (isCodeValid) {
+            console.log('Code valide. Faites ce que vous devez faire ici.')
+          } else {
+            console.log('Code invalide')
+          }
+        }
+      })
+    } catch (error) {
+      console.error('Erreur lors de la récupération des participants', error)
+    }
   }
 
   return (
