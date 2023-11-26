41f7268449d7d7ce9565d2bee5dec898f54e393c
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-26 22:15:12 +0100

    ADD style for join page

--- src/routes/Join/Join.jsx
+++ src/routes/Join/Join.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import HeaderCard from '../../layout/HeaderCard/HeaderCard'
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+
 import './Join.scss'
 
 export default function Join() {
-  return <div>Join</div>
+  return (
+    <div className="join">
+      <HeaderCard secondaryTitle={'Enter your verification code'} />
+      <p>We sent the code in the email of invitation</p>
+      <Input
+        placeholder={'Code'}
+        name="code"
+        id="code"
+        type="string"
+        required={true}
+      />
+      <Button className="button__color--primary">Submit</Button>
+    </div>
+  )
 }
