81fcc2718d3135200ce1572fd3d5a1371ab06ded
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-27 13:53:03 +0100

    FIX responsive

--- src/routes/TermsOfUse/TermsOfUse.jsx
+++ src/routes/TermsOfUse/TermsOfUse.jsx
@@ -18,42 +18,54 @@ function TermsOfUse() {
         <li>
           <strong>Acceptance of Terms</strong>
           <br />
-          By accessing this website, you agree to be bound by these Terms of Use
-          and all applicable laws and regulations. If you do not agree to these
-          terms, please do not use this site.
+          <p>
+            By accessing this website, you agree to be bound by these Terms of
+            Use and all applicable laws and regulations. If you do not agree to
+            these terms, please do not use this site.
+          </p>
         </li>
         <li>
           <strong>Use of the Site</strong>
           <br />
-          You are authorized to access our website for personal and
-          non-commercial use. You agree not to use the site for any illegal or
-          prohibited purposes under these terms.
+          <p>
+            You are authorized to access our website for personal and
+            non-commercial use. You agree not to use the site for any illegal or
+            prohibited purposes under these terms.
+          </p>
         </li>
         <li>
           <strong>Intellectual Property</strong>
           <br />
-          The content of the site, including but not limited to text, graphics,
-          logos, images, audio and video clips, is the exclusive property of
-          Ho-Ho! and is protected by intellectual property laws.
+          <p>
+            The content of the site, including but not limited to text,
+            graphics, logos, images, audio and video clips, is the exclusive
+            property of Ho-Ho! and is protected by intellectual property laws.
+          </p>
         </li>
         <li>
           <strong>User Content</strong>
           <br />
-          By submitting content to the site, you grant Ho-Ho! a non-exclusive,
-          worldwide, irrevocable, transferable, and royalty-free license to use,
-          reproduce, distribute, and display this content.
+          <p>
+            By submitting content to the site, you grant Ho-Ho! a non-exclusive,
+            worldwide, irrevocable, transferable, and royalty-free license to
+            use, reproduce, distribute, and display this content.
+          </p>
         </li>
         <li>
           <strong>Limitation of Liability</strong>
           <br />
-          Ho-Ho! does not guarantee the accuracy, completeness, or relevance of
-          the site's content. The use of the site is at your own risk.
+          <p>
+            Ho-Ho! does not guarantee the accuracy, completeness, or relevance
+            of the site's content. The use of the site is at your own risk.
+          </p>
         </li>
         <li>
           <strong>Modification of Terms</strong>
           <br />
-          Ho-Ho! reserves the right to modify these Terms of Use at any time. It
-          is your responsibility to periodically check for changes.
+          <p>
+            Ho-Ho! reserves the right to modify these Terms of Use at any time.
+            It is your responsibility to periodically check for changes.
+          </p>
         </li>
       </ul>
     </div>
