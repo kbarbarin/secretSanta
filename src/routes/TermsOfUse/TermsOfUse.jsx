e90cb867d07b194c53b5b4b45048baa6c4508ba2
Lucas Miranda <lmiranda@corp.free.fr>
2023-11-27 09:49:56 +0100

    ADD Use of the Site

--- src/routes/TermsOfUse/TermsOfUse.jsx
+++ src/routes/TermsOfUse/TermsOfUse.jsx
@@ -13,6 +13,13 @@ function TermsOfUse() {
           and all applicable laws and regulations. If you do not agree to these
           terms, please do not use this site.
         </li>
+        <li>
+          <strong>Use of the Site</strong>
+          <br />
+          You are authorized to access our website for personal and
+          non-commercial use. You agree not to use the site for any illegal or
+          prohibited purposes under these terms.
+        </li>
       </ul>
     </div>
   )
