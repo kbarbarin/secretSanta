f51608f0e1e9a40abae1221ead7d22ead10f2ca8
Lucas Miranda <lmiranda@corp.free.fr>
2023-11-27 09:49:18 +0100

    ADD Acceptance of Terms

--- src/routes/TermsOfUse/TermsOfUse.jsx
+++ src/routes/TermsOfUse/TermsOfUse.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import './TermsOfUse.scss'
 
 function TermsOfUse() {
-  return <div>TermsOfUse</div>
+  return (
+    <div className="termsOfUseContainer">
+      <h1>Terms of Use</h1>
+      <ul>
+        <li>
+          <strong>Acceptance of Terms</strong>
+          <br />
+          By accessing this website, you agree to be bound by these Terms of Use
+          and all applicable laws and regulations. If you do not agree to these
+          terms, please do not use this site.
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default TermsOfUse
