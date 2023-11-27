794c9798783c70c7248aebd6c3f9518479acfd19
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2023-11-28 00:32:12 +0100

    FIX router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -41,6 +41,8 @@ const routes = [
       {
         path: 'CreateSecretSanta',
         element: <CreateSecretSanta />,
+      },
+      {
         path: '/forgottenpassword',
         element: <ForgottenPassword />,
         errorElement: <ErrorPage />,
