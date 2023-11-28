adbc22b68411b50a7b3f8d18075004e5fb18a8a8
Lucas Miranda <lmiranda@corp.free.fr>
2023-11-28 15:55:47 +0100

    ADD static product when full answer no

--- src/routes/GiftIdeas/GiftIdeas.jsx
+++ src/routes/GiftIdeas/GiftIdeas.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import './GeaftIdeas.scss'
 
 const GeaftIdeas = ({ priceRange, recommandations }) => {
+  const products = [
+    {
+      imageUrl:
+        'https://img01.ztat.net/article/spp-media-p1/448dce44b0473605b18626a833de76cd/1615781d04574f9aba280ace56c7ba14.jpg?imwidth=1800&filter=packshot',
+      price: '15',
+      name: 'Echarpe',
+    },
+    {
+      imageUrl:
+        'https://www.boutique-artisans-du-monde.com/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/m/a/mai556.jpg',
+      price: '10',
+      name: 'Tasse de Noël',
+    },
+    {
+      imageUrl:
+        'https://www.laboutiqueduscoutisme.com/5145-large_default/bonnet-rouge-avec-pompom.jpg',
+      price: '5',
+      name: 'Bonnet',
+    },
+  ]
   return (
     <div className="recommendations">
       <div>
@@ -40,6 +60,27 @@ const GeaftIdeas = ({ priceRange, recommandations }) => {
               )}
             </div>
           ))}
+          <div className="recommendations-info">
+            {products.map((product, index) => (
+              <div key={index} className="recommendation-product">
+                <div className="box-product">
+                  {product.imageUrl && (
+                    <img
+                      src={product.imageUrl}
+                      alt={`Product ${index}`}
+                      className="recommendations-image"
+                    />
+                  )}
+                </div>
+                <p className="recommendation-name">{product.name}</p>
+                {product.price && (
+                  <p className="recommendation-price">
+                    Price: {product.price}€
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -47,4 +88,3 @@ const GeaftIdeas = ({ priceRange, recommandations }) => {
 }
 
 export default GeaftIdeas
-
