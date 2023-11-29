f2197d37a7f333bbdbab8538d5c0b45fdb785741
lsun <lsun.github@gmail.com>
2023-11-29 11:46:36 +0100

    add: image for each question

--- src/datas/questions.js
+++ src/datas/questions.js
@@ -81,12 +81,16 @@ export const questions = [
     text: 'Do you prefer pop music?',
     category: 'musics',
     theme: 'pop',
+    imageUrl:
+      'https://col-ill-illfurth.site.ac-strasbourg.fr/blog/wp-content/uploads/2011/03/sticker-casque-pop-music.jpg',
   },
   {
     id: 7,
     text: 'Do you prefer classic music?',
     category: 'musics',
     theme: 'classic',
+    imageUrl:
+      'https://www.gbnews.ch/wp-content/gbnews-uploads/2013/10/classical-music.jpg',
   },
   {
     id: 8,
@@ -132,31 +136,39 @@ export const questions = [
     text: 'Do you prefer yoga equipment?',
     category: 'sports',
     theme: 'yoga',
+    imageUrl:
+      'https://assets-global.website-files.com/63d13f761f56b471e8ba4f7e/6400c48b6989e31ce73c9bef_7_1_6-1.webp',
   },
   {
     id: 14,
     text: 'Do you prefer gym equipment?',
     category: 'sports',
     theme: 'gym',
+    imageUrl:
+      'https://static.actu.fr/uploads/2020/10/salle-de-sport-fermeture-covid19-coronavirus-paris-iledefrance-perfecture-de-police-1.jpeg',
   },
   {
     id: 15,
     text: 'Do you prefer sport games?',
     category: 'video games',
     theme: 'sport',
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2013/07/12/15/36/handball-150163_960_720.png',
   },
   {
     id: 16,
     text: 'Do you prefer rap music?',
     category: 'musics',
     theme: 'rap',
+    imageUrl:
+      'https://play-lh.googleusercontent.com/3wReLLObI6rQP0QEut9h_9qR2AVBpZ7iBptTmnDH-Z3gpwTOtK-ZddV2oo6-ZUX4yhQ',
   },
   {
     id: 17,
     text: 'Do you like movies?',
     category: 'movies',
     imageUrl:
-      '',
+      'https://media.istockphoto.com/id/911590226/vector/movie-time-vector-illustration-cinema-poster-concept-on-red-round-background-composition-with.jpg?s=612x612&w=0&k=20&c=QMpr4AHrBgHuOCnv2N6mPUQEOr5Mo8lE7TyWaZ4r9oo=',
   },
   {
     id: 18,
@@ -164,109 +176,61 @@ export const questions = [
     category: 'video games',
     theme: 'RPG',
     imageUrl:
-      '',
+      'https://play-lh.googleusercontent.com/lahVHSlMNgsEefAV1xN-cgx6RbwGGnTlwzJ6cwqPjXeom2tOAgRAL12ChdtgCG8OPfE',
   },
   {
     id: 18,
     text: 'Do you prefer concert?',
     category: 'musics',
     theme: 'concert',
-    imageUrl:
-      '',
+    imageUrl: 'https://assets.unileversolutions.com/v1/985812.jpg',
   },
   {
     id: 19,
-    text: 'Do you prefer concert?',
-    category: 'musics',
-    theme: 'concert',
-    imageUrl:
-      '',
-  },
-  {
-    id: 20,
     text: 'Do you prefer tennis equipement?',
     category: 'sports',
     theme: 'tennis',
     imageUrl:
-      '',
+      'https://media.istockphoto.com/id/1345493117/fr/vectoriel/ic%C3%B4ne-de-raquette-de-tennis-vecteur-stock-logo-de-tennis-isol%C3%A9-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=Za0kR-jkoOym9gg0rnLViZEaCRtqOQXUjB0GjtS_H2o=',
   },
   {
-    id: 21,
+    id: 20,
     text: 'Do you prefer detective books?',
     category: 'books',
     theme: 'detective',
     imageUrl:
-      '',
+      'https://cdn.pixabay.com/photo/2021/08/17/06/32/detective-6552137_960_720.jpg',
   },
   {
-    id: 22,
+    id: 21,
     text: 'Do you prefer boxe equipement?',
     category: 'sports',
     theme: 'boxe',
     imageUrl:
-      '',
+      'https://img.freepik.com/vecteurs-libre/gants-boxe-suspendus-materiel-competition-protection-main-illustration-vectorielle_1284-41868.jpg',
   },
   {
-    id: 23,
+    id: 22,
     text: 'Do you prefer comedie movies?',
     category: 'movies',
     theme: 'comedie',
     imageUrl:
-      '',
+      'https://media.istockphoto.com/id/1317000702/photo/comedy-movie-genre-concept-film-reel-forming-the-red-comedy-word-on-white-background.jpg?s=612x612&w=0&k=20&c=fS0Aa4HtXtHzUwQkocUs6500RsqEl8Exc9uYvOWKV_s=',
   },
   {
-    id: 24,
+    id: 23,
     text: 'Do you prefer action movies?',
     category: 'movies',
     theme: 'action',
     imageUrl:
-      '',
+      'https://w7.pngwing.com/pngs/422/671/png-transparent-action-film-cinema-computer-icons-action-movie-drama-film-action-film-thumbnail.png',
   },
   {
-    id: 25,
+    id: 24,
     text: 'Do you prefer horror movies?',
     category: 'movies',
     theme: 'horror',
     imageUrl:
-      '',
-  },
-  {
-    id: 26,
-    text: 'Do you like board games?',
-    category: 'board games',
-    imageUrl:
-      '',
-  },
-  {
-    id: 27,
-    text: 'Do you prefer deck building board games?',
-    category: 'board games',
-    theme: 'deck building',
-    imageUrl:
-      '',
-  },
-  {
-    id: 28,
-    text: 'Do you prefer abstract board games?',
-    category: 'board games',
-    theme: 'abstract',
-    imageUrl:
-      '',
-  },
-  {
-    id: 29,
-    text: 'Do you prefer bluffing board games?',
-    category: 'board games',
-    theme: 'bluffing',
-    imageUrl:
-      '',
-  },
-  {
-    id: 30,
-    text: 'Do you prefer adventure movies?',
-    category: 'movies',
-    theme: 'adventure',
-    imageUrl:
-      '',
+      'https://media.istockphoto.com/id/185777808/photo/mask-movie-panic.jpg?s=612x612&w=0&k=20&c=NnslAmeau6Rc2grBFNRwlXk-CedRw-IYqCG3vv-R_ho=',
   },
 ]
